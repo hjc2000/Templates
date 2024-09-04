d509051247008a7b39c8b059c695b80abd3f630f
hjc2000 <hjc.666@foxmail.com>
2024-09-04 08:52:36 +0800

    Initial commit on new branch without history

---
+++ ProjectTemplates/WpfBlazorTemplate/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace $safeprojectname$;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        ServiceCollection serviceCollection = new();
+        serviceCollection.AddWpfBlazorWebView();
+        Resources.Add("services", serviceCollection.BuildServiceProvider());
+    }
+}
